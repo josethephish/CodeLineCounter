c7f2e2c0817bd100974ebd16b31fe18a53790802
josethephish <109689015+josethephish@users.noreply.github.com>
2024-07-12 16:43:00 -0400

    CodeFile.py

---
+++ CodeFile.py
@@ -0,0 +1,13 @@
+import unittest
+from code_line_counter import CodeLineCounter
+
+class TestCodeLineCounter(unittest.TestCase):
+    def test_should_count_code_lines(self):
+        code = "int a = 0;\n# this is a comment\n\nint b = 1;"
+        counter = CodeLineCounter()
+        result = counter.count(code)
+        self.assertEqual(result, 2)  # Solo las líneas de código reales deben contarse
+
+if __name__ == '__main__':
+    unittest.main()
+
